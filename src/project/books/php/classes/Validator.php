2d083fa331720891e891f53128a6d7bb1ca58656
Rory OBriain <n00255108@iadt.ie>
2026-04-15 11:54:36 +0100

    updated validator. apparently there was a bug

--- src/project/books/php/classes/Validator.php
+++ src/project/books/php/classes/Validator.php
@@ -112,7 +112,11 @@ class Validator {
     }
 
     private function validateRequired($field, $value) {
-        if ($value === null || $value === '' || (is_array($value) && empty($value)) || (is_array($value) && array_key_exists("error", $value) && $value["error"] === UPLOAD_ERR_NO_FILE)) {
+        if ($value === null || 
+            $value === '' || 
+            (is_array($value) && empty($value)) || 
+            (is_array($value) && array_key_exists('error', $value) && $value['error'] === UPLOAD_ERR_NO_FILE))
+        {
             $this->addError($field, "The $field field is required.");
         }
     }
